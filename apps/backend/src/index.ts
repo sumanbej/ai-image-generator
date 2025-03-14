d0be263a11ed77d6384892b4a0af4ce698da6ea7
Suman Bej <sumanbej.glt@gmail.com>
2025-03-14 16:48:37 +0530

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,11 @@
+import express,{Request,Response} from 'express';
+
+const app = express();
+const port = 4000;  
+app.get('/', (req : Request, res: Response) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
