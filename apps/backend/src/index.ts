ae009139038b63f399b7f5ade9da826392ae8a97
Suman Bej <sumanbej.glt@gmail.com>
2025-03-16 00:50:03 +0530

    db migrate

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express,{Request,Response} from 'express';
 import 'dotenv/config';
 import { TrainModel, GenerateImage, GenerateImagesFromPack } from "common/types";
+import { prismaClient } from 'db';
 
 const PORT : number = parseInt(process.env.PORT as string,10) || 7072;
 
