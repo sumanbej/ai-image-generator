48d6a26027fa95efe50943cd7288440f70783074
Suman Bej <sumanbej.glt@gmail.com>
2025-03-15 23:23:41 +0530

    changes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,14 @@
 import express,{Request,Response} from 'express';
+import 'dotenv/config';
+
+const PORT : number = parseInt(process.env.PORT as string,10) || 7072;
 
 const app = express();
-const port = 4000;  
+const port = PORT; 
 app.get('/', (req : Request, res: Response) => {
   res.send('Hello World!');
 })
+app.post()
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
