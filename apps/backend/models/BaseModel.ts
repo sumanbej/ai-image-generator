423c47107299e21b6304e147aa8c2a72f99caf24
Suman Bej <sumanbej.glt@gmail.com>
2025-03-24 23:26:49 +0530

    added fal-ai setup

---
+++ apps/backend/models/BaseModel.ts
@@ -0,0 +1,12 @@
+export class BaseModel {  
+  constructor() {}
+   
+
+private async generateImages(prompt :string, tensorPath :string ){
+
+}  
+private async trainModel(inputImages :string[],triggerWord :string){
+
+} 
+
+}
